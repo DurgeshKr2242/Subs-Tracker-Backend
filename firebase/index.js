e36b0b29e137deff912483e57496e37c3a00cb31
DurgeshKr2242 <code.durgesh@gmail.com>
2022-01-30 08:51:23 +0530

    securing fbServiceAccnKey

--- firebase/index.js
+++ firebase/index.js
@@ -3,7 +3,12 @@ var admin = require("firebase-admin");
 var serviceAccount = require("../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  // credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
+    private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    clientEmail: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+  }),
 });
 
 module.exports = admin;
